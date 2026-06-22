25b21f10ff8ceadeb722199588ea1d5f0c3ef128
NimmraTariq <nimra369tariq@gmail.com>
2026-06-22 12:30:33 +0700

    Add Import to Database functionality

---
+++ CRUDMahasiswaADO/ListMahasiswa.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace CRUDMahasiswaADO
+{
+    public class ListMahasiswa : List<MahasiswaData>
+    {
+    }
+}
