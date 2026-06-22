7822493df55c722695e8784191438f2d368108e6
NimmraTariq <nimra369tariq@gmail.com>
2026-06-22 12:30:29 +0700

    Create sp_Report stored procedure for Rekap Data

---
+++ CRUDMahasiswaADO/RekapMahasiswa.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace CRUDMahasiswaADO
+{
+    public partial class RekapMahasiswa : Form
+    {
+        string connectionString = "Data Source=Nimra\\NIMRA;Initial Catalog=DBAkademikADO;Integrated Security=True";
+        SqlConnection conn;
+        SqlDataAdapter da;
+        DataTable dtMahasiswa;
+        DataTable dtProdi;
+
+        public RekapMahasiswa()
+        {
+            InitializeComponent();
+            conn = new SqlConnection(connectionString);
+        }
+
+        private void RekapMahasiswa_Load(object sender, EventArgs e)
+        {
+            dtpTanggalMasuk.Format = DateTimePickerFormat.Custom;
+            dtpTanggalMasuk.CustomFormat = "yyyy";
+            dtpTanggalMasuk.ShowUpDown = true;
+            dtpTanggalMasuk.MinDate = new DateTime(2000, 1, 1);
+            dtpTanggalMasuk.MaxDate = DateTime.Now;
+
+            cmbProdi.DropDownStyle = ComboBoxStyle.DropDownList;
+            btnCetak.Enabled = false;
+
+            try
+            {
+                using (SqlConnection freshConn = new SqlConnection(connectionString))
+                {
+                    freshConn.Open();
+
+                    SqlCommand cmd = new SqlCommand("select NamaPS from programstudi", freshConn);
+                    cmd.CommandType = CommandType.Text;
+
+                    dtProdi = new DataTable();
+                    SqlDataAdapter freshDa = new SqlDataAdapter(cmd);
+                    freshDa.Fill(dtProdi);
+
+                    cmbProdi.DataSource = dtProdi;
+                    cmbProdi.DisplayMember = "NamaPS";
+                    cmbProdi.ValueMember = "NamaPS";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                    conn.Open();
+
+                SqlCommand cmd = new SqlCommand("sp_Report", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@inProdi", SqlDbType.VarChar, 50).Value = cmbProdi.SelectedValue;
+                cmd.Parameters.Add("@inTglMsuk", SqlDbType.VarChar, 4).Value = dtpTanggalMasuk.Value.Year.ToString();
+
+                da = new SqlDataAdapter(cmd);
+                dtMahasiswa = new DataTable();
+                da.Fill(dtMahasiswa);
+
+                dataGridView1.DataSource = dtMahasiswa;
+
+                if (dtMahasiswa.Rows.Count > 0)
+                {
+                    btnCetak.Enabled = true;
+                }
+                else
+                {
+                    btnCetak.Enabled = false;
+                    MessageBox.Show("Data tidak ditemukan");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Gagal load data: " + ex.Message);
+            }
+        }
+
+        private void btnCetak_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Hide();
+                Report frm2 = new Report(cmbProdi.SelectedValue.ToString(), dtpTanggalMasuk.Value);
+                frm2.ShowDialog();
+                this.Show();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+                this.Show();
+            }
+        }
+    }
+}
